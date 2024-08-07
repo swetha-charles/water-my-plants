7d8e9aca49e2ba21ba26a07093e825459791ef25
Swetha Charles <swethacharles10@gmail.com>
2024-08-07 13:36:53 +0100

    Make plant cards clickable

--- src/components/Plant.tsx
+++ src/components/Plant.tsx
@@ -1,21 +1,23 @@
-import classes from './Plant.module.css'
+import { Link } from "react-router-dom";
+import classes from "./Plant.module.css";
 
-type PlantProps = {
+export type PlantProps = {
+  id: string;
   name: string;
   type: string;
   image: string;
   water: string;
 };
 
-export default function Plant({ name, type, water, image }: PlantProps) {
+export default function Plant({ id, name, type, water, image }: PlantProps) {
   return (
-    <div className={classes.plant}>
-      <img className={classes.plantImage} src={image} />
-      <div>
+    <li className={classes.plant}>
+      <Link to={id}>
+        <img className={classes.plantImage} src={image} />
         <h2> {name} </h2>
-      </div>
-      <div>Water needs: {water}</div>
-      <div>Type: {type}</div>
-    </div>
+        <div>Water needs: {water}</div>
+        <div>Type: {type}</div>
+      </Link>
+    </li>
   );
 }
