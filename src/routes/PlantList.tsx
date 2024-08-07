474f5e9f078b956864c5119d7a677f270f36a9d1
Swetha Charles <swethacharles10@gmail.com>
2024-08-07 13:31:13 +0100

    Add key for items in list

--- src/routes/PlantList.tsx
+++ src/routes/PlantList.tsx
@@ -1,22 +1,17 @@
 import { useEffect, useState } from "react";
 import Plant from "./../components/Plant";
 import classes from "./PlantList.module.css";
-
-type PlantType = {
-  name: string;
-  image: string;
-  type: string;
-  water: string;
-}
+import { PlantProps } from "./../components/Plant";
+import { Outlet } from "react-router-dom";
 
 export default function PlantList() {
-  const [plants, setPlants] = useState<PlantType[]>([]);
+  const [plants, setPlants] = useState<PlantProps[]>([]);
 
   useEffect(() => {
     const getPlants = async () => {
       const response = await fetch("http://localhost:8080/plants");
       const data = await response.json();
-      return data.plants as PlantType[];
+      return data.plants as PlantProps[];
     };
 
     getPlants()
@@ -25,14 +20,17 @@ export default function PlantList() {
   }, []);
 
   return (
-    <div>
-      {plants.length > 0 && (
-        <ul className={classes.plantList}>
-          {plants.map((plant) => (
-            <Plant {...plant} />
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <Outlet />
+      <div>
+        {plants.length > 0 && (
+          <ul className={classes.plantList}>
+            {plants.map((plant) => (
+              <Plant key={plant.id} {...plant} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
