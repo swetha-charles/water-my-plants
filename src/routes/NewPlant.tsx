b553f2809611816f91a471a0919d6fb47aa5b6ab
Swetha Charles <swethacharles10@gmail.com>
2024-09-06 16:53:59 +0100

    Add handler to add new plant

--- src/routes/NewPlant.tsx
+++ src/routes/NewPlant.tsx
@@ -1,13 +1,37 @@
+import { FormEvent } from "react";
 import { Modal } from "../components/Modal";
 import styles from "./NewPlant.module.css";
 
 export default function NewPlant() {
+  const handleClick = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Array.from(formData.entries()).reduce((acc, entry) => ({...acc, [entry[0]]: entry[1]}), {})
+    const body = JSON.stringify(data);
+
+    try {
+      const result = await fetch("http://localhost:8080/plants", {
+        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        body,
+      });
+      console.log(result);
+    } catch (e) {
+      console.log("error!!!", e);
+    }
+  };
+
   return (
     <Modal>
-      <form className={styles.newPlantForm}>
+      <form onSubmit={handleClick} className={styles.newPlantForm}>
         <div className={styles.newPlantDetail}>
           <label htmlFor="name">Enter plant name:</label>
           <input name="name" type="text" />
+        </div>
+        <div className={styles.newPlantDetail}>
+          <label htmlFor="image">Enter plant image URL:</label>
+          <input name="image" type="url" />
         </div>
         <div className={styles.newPlantDetail}>
           <label htmlFor="type">Enter plant type:</label>
@@ -16,12 +40,12 @@ export default function NewPlant() {
         <div className={styles.newPlantDetail}>
           <label htmlFor="water">Choose watering schedule:</label>
           <select name="water">
-            <option value="1">Once a week</option>
-            <option value="2">Once every 2 weeks</option>
-            <option value="4">Once every 4 weeks</option>
+            <option value="high">Once a week</option>
+            <option value="med">Once every 2 weeks</option>
+            <option value="low">Once every 4 weeks</option>
           </select>
         </div>
-        <button type="submit">Add new plant</button>
+        <button>Add new plant</button>
       </form>
     </Modal>
   );
