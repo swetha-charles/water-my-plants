f9e158a3761688d44bf12676459bc39025c34118
Swetha Charles <swethacharles10@gmail.com>
2024-09-06 17:18:17 +0100

    Fix data error & add buttons to submit and cancel

--- src/routes/NewPlant.tsx
+++ src/routes/NewPlant.tsx
@@ -1,18 +1,29 @@
 import { FormEvent } from "react";
 import { Modal } from "../components/Modal";
 import styles from "./NewPlant.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function NewPlant() {
-  const handleClick = async (event: FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate()
+
+  function closeHandler() {
+    // programmatic navigation
+    navigate('..')
+  }
+  
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
-    const data = Array.from(formData.entries()).reduce((acc, entry) => ({...acc, [entry[0]]: entry[1]}), {})
+    const data = Array.from(formData.entries()).reduce(
+      (acc, entry) => ({ ...acc, [entry[0]]: entry[1] }),
+      {}
+    );
     const body = JSON.stringify(data);
 
     try {
       const result = await fetch("http://localhost:8080/plants", {
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
         method: "POST",
         body,
       });
@@ -24,7 +35,7 @@ export default function NewPlant() {
 
   return (
     <Modal>
-      <form onSubmit={handleClick} className={styles.newPlantForm}>
+      <form onSubmit={handleSubmit} className={styles.newPlantForm}>
         <div className={styles.newPlantDetail}>
           <label htmlFor="name">Enter plant name:</label>
           <input name="name" type="text" />
@@ -45,7 +56,10 @@ export default function NewPlant() {
             <option value="low">Once every 4 weeks</option>
           </select>
         </div>
-        <button>Add new plant</button>
+        <div className={styles.newPlantDetail}>
+          <label><button type="submit">Add new plant</button></label>
+          <label><button onClick={closeHandler}>Close</button></label>
+        </div>
       </form>
     </Modal>
   );
